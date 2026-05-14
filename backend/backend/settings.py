a8aab7740c04d1b485f4a2e862884f82b3bc42d7
leonid135 <zimin-leonid1999@mail.ru>
2026-05-14 20:23:56 +0300

    test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -229,4 +229,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-GROQ_API_KEY = open("../../secret.txt").read()
+# GROQ_API_KEY = open("../../secret.txt").read()
+GROQ_API_KEY = ""
